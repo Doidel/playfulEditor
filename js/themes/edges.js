d5fd7aec82b9157f86138d6d0ac0d818ea69af27
Danilo Wanner <danilo.wanner@gmail.com>
2014-11-11 16:14:06 +0100

    added primitives to edges theme

--- js/themes/edges.js
+++ js/themes/edges.js
@@ -56,13 +56,85 @@ _lS = {
 	
 	},
 	
+	
 	prefabsList: {
 		// name: function to create it
+		'Box': function ( ) {
+			var width = 1;
+			var height = 1;
+			var depth = 1;
+	
+			var widthSegments = 1;
+			var heightSegments = 1;
+			var depthSegments = 1;
+	
+			var geometry = new THREE.BoxGeometry( width, height, depth, widthSegments, heightSegments, depthSegments );
+			var material = Physijs.createMaterial(
+				new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading }  ),
+				0.5,
+				0.5
+			);
+			var mesh = new Physijs.BoxMesh( geometry, material );
+			
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
+			mesh.isStatic = true;
+			
+			return mesh;
+		},
+		'Sphere': function ( ) {
+			var radius = 0.5;
+			var widthSegments = 32;
+			var heightSegments = 16;
+	
+			var geometry = new THREE.SphereGeometry( radius, widthSegments, heightSegments );
+			var material = Physijs.createMaterial(
+				new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading }  ),
+				0.5,
+				0.5
+			);
+			var mesh = new Physijs.SphereMesh( geometry, material );
+			
+			// Enable CCD if the object moves more than 1 meter in one simulation frame
+			mesh.setCcdMotionThreshold(1);
+			// Set the radius of the embedded sphere such that it is smaller than the object
+			mesh.setCcdSweptSphereRadius(0.2);
+			
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
+			mesh.isStatic = true;
+	
+			editor.addObject( mesh );
+			editor.select( mesh );
+		},
+		'Cylinder': function ( ) {
+			var radiusTop = 0.5;
+			var radiusBottom = 0.5;
+			var height = 1;
+			var radiusSegments = 8;
+			var heightSegments = 1;
+			var openEnded = false;
+	
+			var geometry = new THREE.CylinderGeometry( radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded );
+			var material = Physijs.createMaterial(
+				new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading }  ),
+				0.5,
+				0.5
+			);
+			var mesh = new Physijs.CylinderMesh( geometry, material );
+			
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
+			mesh.isStatic = true;
+	
+			editor.addObject( mesh );
+			editor.select( mesh );
+		},
 		'Resurrection Sphere': function ( ) {
 			var mesh = new Physijs.SphereMesh(
 				new THREE.SphereGeometry( 0.5, 32, 16 ),
 				Physijs.createMaterial(
-					new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading }  ),
+					new THREE.MeshPhongMaterial( { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 10, shading: THREE.FlatShading }  ),
 					0.5,
 					0.5
 				)
